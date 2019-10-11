cfd40cfb68724745918684d521190605d45d395e
jckent <chasekent30@gmail.com>
2019-10-11 11:03:38 -0400

    added axios pull for movie-this

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var axios = require('axios');
-var search= 
+var search= process.argv.splice(3, process.argv.length - 1);
 
 switch (process.argv[2]) {
     case 'concert-this':
@@ -16,7 +16,18 @@ switch (process.argv[2]) {
         break;
 
     case 'movie-this':
-        //axios to omdb;
+        axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log('The movie is called: ' + response.data.Title);
+    console.log('The movie came out in: ' + response.data.Year);
+    console.log("The movie's rating is: " + response.data.imdbRating);
+    console.log("The movie's Rotten Tomatoes rating is: " + response.data.Metascore);
+    console.log('The movie was made in: ' + response.data.Country);
+    console.log('The movie was recorded in: ' + response.data.Language);
+    console.log('The following people appeared in the movie: ' + response.data.Actors);
+    console.log('The plot is: ' + response.data.Plot);
+
+  });
         //console.log results
         break;
 
