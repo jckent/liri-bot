093cf5a726059c4fe8c78f2cb0b4644d4de98b3f
jckent <chasekent30@gmail.com>
2019-10-13 05:47:05 -0400

    added spotify code

--- liri.js
+++ liri.js
@@ -1,41 +1,55 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify= require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var axios = require('axios');
 var moment = require('moment');
-var search= process.argv.splice(3, process.argv.length - 1);
+var search = process.argv.splice(3, process.argv.length - 1);
 
 switch (process.argv[2]) {
     case 'concert-this':
         axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp").then(
-            function(response) {
+            function (response) {
                 var correctFormat = (moment("response.data.datetime", "MM-DD-YYYY"))
                 console.log('The venue is called: ' + response.data.venue.name);
                 console.log('The venue is in: ' + response.data.venue.city);
                 console.log('The concert is on: ' + correctFormat);
             }
         )
-        
+
         break;
-    
+
     case 'spotify-this-song':
-        //axios to spotify;
-        //console.log results
+        spotify
+            .search({
+                type: 'track',
+                query: search
+            })
+            .then(function (response) {
+                console.log(response.data.artists);
+                console.log(response.data.track.name);
+                console.log(response.data.track.album);
+                console.log(response.data.track.preview_url);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+        
         break;
 
     case 'movie-this':
         axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log('The movie is called: ' + response.data.Title);
-    console.log('The movie came out in: ' + response.data.Year);
-    console.log("The movie's rating is: " + response.data.imdbRating);
-    console.log("The movie's Rotten Tomatoes rating is: " + response.data.Metascore);
-    console.log('The movie was made in: ' + response.data.Country);
-    console.log('The movie was recorded in: ' + response.data.Language);
-    console.log('The following people appeared in the movie: ' + response.data.Actors);
-    console.log('The plot is: ' + response.data.Plot);
+            function (response) {
+                console.log('The movie is called: ' + response.data.Title);
+                console.log('The movie came out in: ' + response.data.Year);
+                console.log("The movie's rating is: " + response.data.imdbRating);
+                console.log("The movie's Rotten Tomatoes rating is: " + response.data.Metascore);
+                console.log('The movie was made in: ' + response.data.Country);
+                console.log('The movie was recorded in: ' + response.data.Language);
+                console.log('The following people appeared in the movie: ' + response.data.Actors);
+                console.log('The plot is: ' + response.data.Plot);
 
-  });
+            });
         break;
 
     case 'do-what-it-says':
@@ -45,4 +59,4 @@ switch (process.argv[2]) {
 
     default:
         console.log('Invalid request');
-}       
+}
